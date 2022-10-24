809af5e84fd0e366da5a75424754f7f983397919
Ximena Campo <32852877+Ximec@users.noreply.github.com>
2022-10-23 21:35:21 -0500

    Creacion del Front

--- back/server.js
+++ back/server.js
@@ -1,10 +1,14 @@
 const app=require("./app")
+const connectDatabase = require("./config/database");
 
-//setear archivo de configuracion
+//Setear el archivo de configuración
 const dotenv=require("dotenv");
-dotenv.config({path:'back/config/config.env'})
+dotenv.config({path: 'back/config/config.env'})
 
-//llamamos al servidor
+//Configurar base de datos
+connectDatabase();
+
+//Llamemos al server
 const server=app.listen(process.env.PORT, () => {
     console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
 })
